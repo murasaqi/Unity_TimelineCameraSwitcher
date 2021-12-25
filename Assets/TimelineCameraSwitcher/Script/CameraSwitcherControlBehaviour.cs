8b80fc2df1b40be0b2b94cf59a070ae819327139
murasaqi <uma9266@gmail.com>
2021-12-25 22:38:26 +0900

    整理

--- Assets/TimelineCameraSwitcher/Script/CameraSwitcherControlBehaviour.cs
+++ Assets/TimelineCameraSwitcher/Script/CameraSwitcherControlBehaviour.cs
@@ -45,20 +45,11 @@ public class CameraSwitcherControlBehaviour : PlayableBehaviour
     [SerializeField] public Vector2 noiseScale = Vector2.one;
     [SerializeField] public float roughness = 1;
     [SerializeField] public Vector2 wiggleRange  = new Vector2(5,5);
-    [SerializeField] public bool dofOverride = false;
     [SerializeField] public Color multiplyColor = Color.white;
-    // [SerializeField] public DepthOfFieldMode depthOfFieldMode;
-    // [SerializeField] public float focusDistance;
-    // [SerializeField, Range(1, 300)] public float focalLength;
-    // [SerializeField, Range(1,32)] public float aperture;
-    // [SerializeField, Range(1,9)] public int bladeCount;
-    // [SerializeField, Range(0, 1)] public float bladeCurvature;
-    // [SerializeField, Range(-180, 180)] public float bladeRotation;
-
+    [SerializeField] public bool dofOverride = false;
+  
     [SerializeField] public DofControlProps dofControlProps;
     [SerializeField] public LookAtProps lookAtProps;
-    
-    // [SerializeField] public Vector2 offsetPosition = Vector2.zero;
     public override void OnPlayableCreate (Playable playable)
     {
        
